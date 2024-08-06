479ebe8d9dcc8e23292b2bd82297576015bb3608
sufyanuddin2288 <sufyanuddin72@gmail.com>
2024-08-06 16:46:51 +0500

    js

---
+++ app.js
@@ -0,0 +1,36 @@
+
+//  chapter1 //
+console.log ("hi")
+alert("hi")
+
+// chapter2//
+var message= "hello world"
+var inp = prompt ("ENTER ANSWER")
+alert("hello world ")
+
+//chapter3//
+alert("I am 18 year old")
+
+//chapter4//
+
+
+//chapter5 addition //
+var a = 50
+var b = 50
+// var C =  a + b 
+console.log(a+b)
+
+//chapter5 subtraction //
+var a = 8
+var b = 5
+console.log(a-b)
+
+//chapter5 multiplication//
+var a = 5
+var b = 5
+console.log(a*b)
+
+//chapter5 division//
+var a = 54
+var b = 6
+console.log(a/b)
